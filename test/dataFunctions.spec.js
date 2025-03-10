82dc7cb40f19d53ffcc1c688bc449c32b1b0db5c
yamileth caldera <yamilethcaldera@gmail.com>
2025-03-10 15:21:43 -0300

    revisando test

--- test/dataFunctions.spec.js
+++ test/dataFunctions.spec.js
@@ -1,28 +1,28 @@
 
-import { sortData, filterData, sortBounty, computeStats, computeStatsBounty } from '../src/lib/dataFunctions.js'
-import { names, crew, arrBounty, origin } from './data.js';
+import { filterData, sortBounty, computeStats, computeStatsBounty } from '../src/lib/dataFunctions.js'
+import { crew, arrBounty, origin } from './data.js';
 
-describe("sortData", () => {
-  it('returns characters sorted by name in ascending or descending order`', () => {
-    const resultNames = [
-      { name: "Eustass Kid" },
-      { name: "Monkey D. Luffy" },
-      { name: "Nami" },
-      { name: "Roronoa Zoro" },
-      { name: "Sir Crocodile" }
-    ];
-    const resultNames2 = [
-      { name: "Sir Crocodile" },
-      { name: "Roronoa Zoro" },
-      { name: "Nami" },
-      { name: "Monkey D. Luffy" },
-      { name: "Eustass Kid" }
-    ];
+// describe("sortData", () => {
+//   it('returns characters sorted by name in ascending or descending order`', () => {
+//     const resultNames = [
+//       { name: "Eustass Kid" },
+//       { name: "Monkey D. Luffy" },
+//       { name: "Nami" },
+//       { name: "Roronoa Zoro" },
+//       { name: "Sir Crocodile" }
+//     ];
+//     const resultNames2 = [
+//       { name: "Sir Crocodile" },
+//       { name: "Roronoa Zoro" },
+//       { name: "Nami" },
+//       { name: "Monkey D. Luffy" },
+//       { name: "Eustass Kid" }
+//     ];
 
-    expect(sortData(names, "name", "asc")).toStrictEqual(resultNames)
-    expect(sortData(names, "name", "desc")).toStrictEqual(resultNames2)
-  });
-});
+//     expect(sortData(names, "name", "asc")).toStrictEqual(resultNames)
+//     expect(sortData(names, "name", "desc")).toStrictEqual(resultNames2)
+//   });
+// });
 
 describe("sortBounty", () => {
   it("returns bounty sorted by numbers in ascending order", () => {
